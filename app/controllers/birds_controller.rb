b8e4f37d58c8aedd4fd027f9fd08f1d5cdf36dae
thistonyb <this.tonybaker@gmail.com>
2019-12-20 14:44:15 -0800

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,7 +1,12 @@
 class BirdsController < ApplicationController
+  # def index
+  #   birds = Bird.all
+  #   render json: birds, only: [:id, :name, :species]
+  # end
+
   def index
     birds = Bird.all
-    render json: birds, only [:id, :name, :species]
+    render json: birds, except: [:created_at, :updated_at]
   end
 
   def show
